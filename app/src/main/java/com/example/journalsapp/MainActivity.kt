2a5a73a6baf62ec1c774393a621f0de32b8aab99
Sarvar <arifdjanovsarvar@gmail.com>
2023-02-17 16:11:06 +0500

    Phase 3
    *Added json parsing functions
    *Added data class journal
    *Added images to cards

--- app/src/main/java/com/example/journalsapp/MainActivity.kt
+++ app/src/main/java/com/example/journalsapp/MainActivity.kt
@@ -1,23 +1,106 @@
 package com.example.journalsapp
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import com.android.volley.DefaultRetryPolicy
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.example.journalsapp.data.Journal
 import com.example.journalsapp.ui.screens.JournalApp
-import com.example.journalsapp.ui.theme.JournalsAppTheme
+import org.json.JSONArray
+import org.json.JSONObject
+import java.nio.charset.Charset
+
+private const val TAG = "MainActivity"
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JournalApp()
+
+            val journalList = remember{
+                mutableStateOf(listOf<Journal>())
+            }
+
+            getData(this@MainActivity,journalList)
+            JournalApp(journalList)
         }
     }
 }
+
+private fun getData(
+    context: Context,
+    journalList: MutableState<List<Journal>>,
+    offset: Int = 0,
+    limit: Int = 10){
+    val url = "http://pressa-api.imb2bs.com/api/v1/journal/categories/0/?" +
+            "offset=$offset" +
+            "&limit=$limit"
+    val requestBody = ""
+
+    val queue = Volley.newRequestQueue(context)
+
+    val stringRequest: StringRequest =
+        object : StringRequest(
+            Method.GET,
+            url,
+            Response.Listener { response ->
+                Log.d(TAG,response)
+                val list = getJournals(response)
+                journalList.value = list
+            },
+            Response.ErrorListener {
+                Log.d(TAG,"VolleyError: $it")
+            }
+        ){
+            override fun getHeaders(): MutableMap<String,String>{
+                val headers = HashMap<String,String>()
+                headers["User-Agent"] = "PostmanRuntime/7.31.0"
+                headers["Project-id"] = "2"
+                headers["Language"] = "ru"
+                return headers
+            }
+
+            override fun getBody(): ByteArray {
+                return requestBody.toByteArray(Charset.defaultCharset())
+            }
+        }
+    queue.add(stringRequest)
+}
+
+private fun getJournals(respose: String): List<Journal>{
+    if (respose.isNullOrEmpty()) return emptyList()
+
+    val jsonArray = JSONArray(respose)
+    val list = ArrayList<Journal>()
+
+    for (i in 0 until jsonArray.length()){
+        val item  = jsonArray[i] as JSONObject
+        list.add(
+            Journal(
+                item.getString("id"),
+                item.getString("name"),
+                item.getString("description"),
+                item.getJSONObject("last_issue").getString("cover")
+            )
+        )
+    }
+    return list
+}
+
+private fun getInfo(list: List<Journal>){
+    list.forEach {
+        Log.d(TAG,"id: ${it.id}")
+        Log.d(TAG,"name: ${it.name}")
+        Log.d(TAG,"desc: ${it.description}")
+        Log.d(TAG,"cover: ${it.cover}")
+    }
+}
+
--- app/src/main/java/com/example/journalsapp/ui/screens/JournalApp.kt
+++ app/src/main/java/com/example/journalsapp/ui/screens/JournalApp.kt
@@ -9,40 +9,44 @@ import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.Card
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import com.example.journalsapp.R
+import com.example.journalsapp.data.Journal
 
-@Preview (showBackground = true)
+
 @Composable
 fun JournalApp(
+    journalList: MutableState<List<Journal>>,
     modifier: Modifier = Modifier
 ){
     LazyVerticalGrid(
         columns = GridCells.Adaptive(150.dp),
     ){
-        items(10){
-            JournalCard()
+        items(journalList.value.size){
+            JournalCard(journalList.value[it])
         }
     }
 }
 
 @Composable
 fun JournalCard(
+    journal: Journal,
     modifier: Modifier = Modifier
 ){
     Card(
         modifier = modifier.padding(4.dp),
         elevation = 4.dp,
     ) {
-        Image(
-            painter = painterResource(id = R.drawable.loading),
-            contentDescription = "loadingImage",
+        AsyncImage(
+            model = journal.cover,
+            contentDescription = journal.name,
             modifier = Modifier.size(200.dp,280.dp),
-            contentScale = ContentScale.Crop,
         )
     }
 }
